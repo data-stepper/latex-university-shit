7983ac0cd363ba27ab52ed3ac61451ce99b86fe7
data-stepper <bent.hochrad@gmail.com>
2021-04-16 00:38:38 +0200

    Bash script added

---
+++ clean_auxilliary_files.sh
@@ -0,0 +1,28 @@
+
+# Shell script to remove all auxiliary files
+# created by latex compiler
+
+rm -rf */*.aux
+rm -rf */*.swp
+rm -rf */*.log
+rm -rf */*.aux
+rm -rf */*.out
+rm -rf */*.gz
+rm -rf */*.bbl
+rm -rf */*.blg
+rm -rf */*.fls
+rm -rf */*.toc
+rm -rf */*.fdb_latexmk
+
+rm -rf */*/*.aux
+rm -rf */*/*.swp
+rm -rf */*/*.log
+rm -rf */*/*.aux
+rm -rf */*/*.out
+rm -rf */*/*.gz
+rm -rf */*/*.bbl
+rm -rf */*/*.blg
+rm -rf */*/*.fls
+rm -rf */*/*.toc
+rm -rf */*/*.fdb_latexmk
+
