fb896c86cccba3168caacffde063f665312d1c85
data-stepper <bent.hochrad@gmail.com>
2021-06-09 13:36:18 +0200

    Unsicherheit bei Aufgabe 26

--- Abgaben/Modellierung/mat.py
+++ Abgaben/Modellierung/mat.py
@@ -9,17 +9,24 @@ A = q * (1 - q)
 B = q * (1 - q)
 E = (1 - q) ** 2 + q ** 2
 
+# p = np.array(
+#     [
+#         [1, 0, 0, 0, 0],
+#         [A, E, B, 0, 0],
+#         [0, A, E, B, 0],
+#         [0, 0, A, E, B],
+#         [0, 0, 0, 0, 1],
+#     ],
+#     dtype=np.float64,
+# )
+
 p = np.array(
-    [
-        [1, 0, 0, 0, 0],
-        [A, E, B, 0, 0],
-        [0, A, E, B, 0],
-        [0, 0, A, E, B],
-        [0, 0, 0, 0, 1],
-    ],
+    [[1, 0, 0, 0], [0.5, 0, 0.5, 0], [0.5, 0, 0, 0.5], [0, 0, 0, 1]],
     dtype=np.float64,
 )
 
-for e in range(1, 500, 200):
-    print("\n")
+print(np.linalg.matrix_power(p, 2))
+
+for e in [2 ** i for i in range(8)]:
+    print("\n" + str(e))
     print(np.linalg.matrix_power(p, e))
