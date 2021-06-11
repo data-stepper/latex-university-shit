f06eb24bd280c79f9d809d0c5598226b14b170d5
data-stepper <bent.hochrad@gmail.com>
2021-06-11 16:09:49 +0200

    Presentation on fire

--- Abgaben/Modellierung/mat.py
+++ Abgaben/Modellierung/mat.py
@@ -1,4 +1,5 @@
 import numpy as np
+import array_to_latex as a2l
 
 # Matrizen zu potenzieren ist leichter in Python ;)
 
@@ -20,12 +21,38 @@ E = (1 - q) ** 2 + q ** 2
 #     dtype=np.float64,
 # )
 
+# p = np.array(
+#     [[1, 0, 0, 0], [0.5, 0, 0.5, 0], [0.5, 0, 0, 0.5], [0, 0, 0, 1]],
+#     dtype=np.float64,
+# )
+
+N = 0.25
+S = 0.5
+
 p = np.array(
-    [[1, 0, 0, 0], [0.5, 0, 0.5, 0], [0.5, 0, 0, 0.5], [0, 0, 0, 1]],
+    [
+        [S + 0.1, N - 0.1, 0, 0, 0, N],
+        [N, S + N, 0, 0, 0, 0],
+        [0, N + 0.15, S, N - 0.15, 0, 0],
+        [0, 0, N + 0.1, S + 0.1, N - 0.2, 0],
+        [0, 0, 0, N, S, N],
+        [N, 0, 0, 0, N, S],
+    ],
     dtype=np.float64,
 )
 
-print(np.linalg.matrix_power(p, 2))
+invar = np.array([0.29, 0.2652, 0.057, 0.0815, 0.1075, 0.1988], dtype=np.float64)
+
+y = print(invar @ p)
+
+p_td = np.linalg.matrix_power(p, 200)
+
+a2l.to_clp(p_td, frmt="{:6.4f}", arraytype="array")
+
+print(p)
+print("\n")
+print(p_td)
+quit()
 
 for e in [2 ** i for i in range(8)]:
     print("\n" + str(e))
